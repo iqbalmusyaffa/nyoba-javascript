84cc78e809e1ffef47d7222a6c9950bf886c1652
iqbalmusyaffa <55908032+iqbalmusyaffa@users.noreply.github.com>
2023-05-14 12:19:28 +0700

    Javascript

---
+++ pembahasan coding syntax/Javascript DOM/Kota.js
@@ -0,0 +1,16 @@
+
+
+function tampilKota(propinsi) {
+    var kota="";
+
+    switch(propinsi) {
+        case "Jabar":{
+            kota = "<ul> <li>Bandung</li> <li>Bogor</li> <li>Garut</li> </ul>";
+        } break;
+        case "Jateng": {
+            kota = "<ul> <li>Cilacap</li> <li>Jepara</li> <li>Magelang</li> </ul>";
+        } break;
+        default: kota = "";
+    }
+    document.getElementById("kota").innerHTML = kota;
+}
